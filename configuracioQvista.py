ea7b090f0fb769ea7150796d2f61f5c19b05eb68
Jordi Cebrián <jcebrian@bcn.cat>
2018-12-16 16:48:28 +0100

    Canvis projecte inicial

--- configuracioQvista.py
+++ configuracioQvista.py
@@ -1,11 +1,13 @@
 import os
+
 #Parametres configuració inicial
-projecteInicial='n:/9siteb/publicacions/qvista/dades/projectes/bcn11_nord.qgs'
+
+titolFinestra = 'qVista 0.1'
 
 carpetaCataleg = "N:/9SITEB/Publicacions/qVista/CatalegProjectes/"
+projecteInicial='n:/9siteb/publicacions/qvista/dades/projectes/bcn11_nord.qgs'
+
 if not os.path.isdir(carpetaCataleg):
     carpetaCataleg = "../dades/CatalegProjectes/"
     projecteInicial = 'mapesOffline/DistrictesMartorell.qgz'
-carpetaCatalegOpenData="d:/dropbox/repsQV/qvista/CatalegOpenData"
-titolFinestra = 'qVista 0.1'
-
+    
