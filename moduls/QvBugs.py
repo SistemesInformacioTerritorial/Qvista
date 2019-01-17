606924da85f1c385b7f83c7d23c311c02bb3fef0
CPCIMI <cpretel@bcn.cat>
2019-01-17 10:42:52 +0100

    Github con usuario qVistaHost

--- moduls/QvBugs.py
+++ moduls/QvBugs.py
@@ -1,7 +1,10 @@
 import requests
 import json
 
-_TOKEN = 'a6ccb2fa7ec5a4a042596ae167d99378196cfacf'
+# USUARI = 'qVistaHost'
+# PASSWORD = 'HostQVista123'
+
+_TOKEN = '830e8b32793cbc2a16f045549cea95aa25e244e4' # qVistaHost
 
 class QvGithub:
 
@@ -24,25 +27,30 @@ class QvGithub:
         try:
             url = self.conn + '/search/issues?q=repo:' + self.repo + '+state:open+label:bug+' + title + '+in:title'
             response = requests.request('GET', url, headers=self.headGet)
-            data = json.loads(response.text)
-            num = data['total_count']
-            return num
+            if response.status_code == 200:
+                data = json.loads(response.text)
+                num = data['total_count']
+                return num
+            else:
+                return -1
         except:
             return -1
     
-    def postBug(self, title, body, users):
+    def postBug(self, title, body, user):
         try:
             url = self.conn + '/repos/' + self.repo + '/issues'
             data = {
                 "title": title,
                 "body": body,
-                "assignees": users,
+                "assignees": [
+                    user
+                ],
                 "labels": [
                     "bug"
                 ]
             }
             response = requests.request('POST', url, json=data, headers=self.headPost)
-            if response.status_code = 201:
+            if response.status_code == 201:
                 return True
             else:
                 return False
@@ -53,25 +61,29 @@ class QvGithub:
         try:
             url = self.conn + '/repos/' + self.repo + '/commits?path=' + path
             response = requests.request('GET', url, headers=self.headGet)
-            data = json.loads(response.text)
-            item = data[0]
-            commit = item['commit']
-            author = commit['author']
-            committer = commit['committer']
-            return author['name'], committer['name']
+            if response.status_code == 200:
+                data = json.loads(response.text)
+                item = data[0]
+                commit = item['commit']
+                author = commit['author']
+                committer = commit['committer']
+                return committer['name']
+            else:
+                return None
         except:
-            return None, None
+            return None
 
 if __name__ == "__main__":
 
     gh = QvGithub()
 
-    ok = gh.postBug('Bug desde app qVista', 'Descripción del error', ['CPCIMI'])
-
     num = gh.getBug('Prueba bug')
     print('Bug:', num)
 
-    autor, commit = gh.getUser('moduls/QvLlegenda.py')
-    print('Usr:', autor, commit)
+    user = gh.getUser('moduls/QvLlegenda.py')
+    print('Usr:', user)
+
+    ok = gh.postBug('Bug desde app qVista', 'Descripción del error', 'CPCIMI')
+
 
 
