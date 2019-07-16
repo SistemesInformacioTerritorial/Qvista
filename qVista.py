432dbc0baf136482aec43410906563d2a35b0958
adalmauWea <aleixdalmau@hotmail.es>
2019-07-16 12:58:49 +0200

    streetView no problemes al dockar

--- qVista.py
+++ qVista.py
@@ -424,7 +424,7 @@ class QVista(QMainWindow, Ui_MainWindow):
         self.dwSV.setContentsMargins ( 0, 0, 0, 0 )
         self.dwSV.hide()
         self.dwSV.visibilityChanged.connect(self.streetViewTancat)
-        # self.addDockWidget( Qt.RightDockWidgetArea, self.dwSV)
+        self.addDockWidget( Qt.RightDockWidgetArea, self.dwSV)
         self.dwSV.setFloating(True)
         self.dwSV.move(575,175)
 
