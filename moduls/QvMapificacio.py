552ed371e85e3d4cc0c81ec22c058dfe3fda6220
CPCIMI <cpretel@bcn.cat>
2019-07-31 14:49:09 +0200

    Input csv y rows

--- moduls/QvMapificacio.py
+++ moduls/QvMapificacio.py
@@ -10,13 +10,13 @@ import csv
 import time
 
 _ZONES = {
-    "Codi postal": "DIST_POST",
+    "Codi postal": "CODI_POSTAL",
     "Districte": "DISTRICTE",
     "Illa": "ILLA",
     "Solar": "SOLAR",
     "Barri": "BARRI",
     "Àrea estadística bàsica": "AEB",
-    "Secció censal": "SECC_CENS",
+    "Secció censal": "SECCIO_CENSAL",
     "Sector policial operatiu": "SPO"
 }
 
@@ -28,24 +28,28 @@ class QvZonificacio(QObject):
         self.input = input
         self.code = code
         self.delimiter = delimiter
-        self.rows = 0
         self.fields = []
+        self.rows = 0
         self.inicio()
 
     def inicio(self):
         if not os.path.isfile(self.input):
             return
-        with open(self.input, encoding=self.code) as csvInput:
-            data = csv.DictReader(csvInput, delimiter=self.delimiter)
-            self.fields = data.fieldnames
-            tot = num = 0
-            size = os.path.getsize(self.input)
-            for row in data:
+        with open(self.input, "r", encoding=self.code) as csvInput:
+            lenFile = os.path.getsize(self.input)
+            data = csvInput.readline()
+            self.fields = data.rstrip(csvInput.newlines).split(self.delimiter)
+            lenMuestra = 0
+            maxMuestra = 60
+            num = 0
+            for data in csvInput:
                 num += 1
-                tot += sys.getsizeof(row)
-                if num == 10:
-                    self.rows = (size * num) // tot
-                    return
+                data = data.encode(self.code)
+                lenMuestra += len(data)
+                if num == maxMuestra:
+                    break
+            lenRow = lenMuestra / num
+            self.rows = int(lenFile // lenRow)
 
             # TODO: Método de cálculo de zonas de Geocod
             # TODO: Probar la geocodificación en una layer directamente
@@ -103,4 +107,10 @@ if __name__ == "__main__":
     with qgisapp(guienabled=gui) as app:
 
         z = QvZonificacio('D:/qVista/CarrecsANSI.csv')
-        print(z.rows, 'filas en ', z.input)
+        print(z.rows, 'filas en', z.input)
+
+        z = QvZonificacio('D:/qVista/CarrecsUTF8.csv', 'UTF-8')
+        print(z.rows, 'filas en', z.input)
+
+        z = QvZonificacio('D:/qVista/GossosBCN.csv')
+        print(z.rows, 'filas en', z.input)
