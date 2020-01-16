60752fa5c07ce280b0dfbad79a1f963145db4e48
CPCIMI <cpretel@bcn.cat>
2020-01-16 14:34:56 +0100

    Token desde config

--- moduls/QvApp.py
+++ moduls/QvApp.py
@@ -97,7 +97,8 @@ class QvApp(Singleton):
         if self.github is None:
             self.gh = None
         else:
-            self.gh = QvGithub(self.data(), self.github)
+            val = self.paramCfg('Token', '')        # Leer Token
+            self.gh = QvGithub(self.data(), self.github, val)
 
         val = self.paramCfg('Stdout', 'False')      # Activación fichero salida
         if val == 'True':
