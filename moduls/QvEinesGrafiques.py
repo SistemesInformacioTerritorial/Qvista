bece1ecd9621adb00ee907225908ed58b82c5078
Oriol Martí i Rodríguez <omarti@nexusgeographics.com>
2020-02-21 09:27:03 +0100

    Per poder fer el merge

--- moduls/QvEinesGrafiques.py
+++ moduls/QvEinesGrafiques.py
@@ -581,8 +581,8 @@ class QvSeleccioElement(QgsMapTool):
                 esquerraDalt = self.canvas.getCoordinateTransform().toMapCoordinates(x-self.radi, y-self.radi)
                 dretaBaix = self.canvas.getCoordinateTransform().toMapCoordinates(x+self.radi, y+self.radi)
             else:
-                esquerraDalt = self.canvas.getCoordinateTransform().toMapCoordinates(x-self.radi*sqrt(2), y-self.radi*sqrt(2))
-                dretaBaix = self.canvas.getCoordinateTransform().toMapCoordinates(x+self.radi*sqrt(2), y-self.radi*sqrt(2))
+                esquerraDalt = self.canvas.getCoordinateTransform().toMapCoordinates(x-self.radi*math.sqrt(2), y-self.radi*math.sqrt(2))
+                dretaBaix = self.canvas.getCoordinateTransform().toMapCoordinates(x+self.radi*math.sqrt(2), y-self.radi*math.sqrt(2))
 
             # marcaLloc = QgsVertexMarker(self.canvas)
             # marcaLloc.setCenter( point )
