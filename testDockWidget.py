86d8c3edfe3cc2d79752fef084dcfc2c3a82f153
CPCIMI <cpretel@bcn.cat>
2020-05-06 11:08:51 +0200

    Workaround para evitar error en dock widgets

--- testDockWidget.py
+++ testDockWidget.py
@@ -13,14 +13,31 @@ projecteInicial = 'MapesOffline/qVista default map.qgs'
 
 with qgisapp() as app:
 
-    
+# QTBUG-65592
+# -----------
+# QDockWidget split position "jumps" when QMainWindow resized
+#
+# Comment:
+#
+# When I encountered this bug it seemed that anything which sets QDockAreaLayoutItem::KeepSize
+# avoided the error. This includes things like undocking and re-docking widgets.
+#
+# My own terribly hacky workaround was thus:
+#
+#     QByteArray temp = saveState();
+#     restoreState(temp);
+#
+# since restoreState sets the flag.
+#
+
     win = QMainWindow()
+    temp = win.saveState()  # Añadido para evitar bug
 
     canvas = QgsMapCanvas()
     
     project = QgsProject.instance()
     root = project.layerTreeRoot()
-    bridge = QgsLayerTreeMapCanvasBridge(root,canvas)
+    bridge = QgsLayerTreeMapCanvasBridge(root, canvas)
 
     win.setCentralWidget(canvas)
     
@@ -29,7 +46,9 @@ with qgisapp() as app:
     dw.setWidget(boton)
     win.addDockWidget(Qt.LeftDockWidgetArea, dw)
     
+    win.restoreState(temp)  # Añadido para evitar bug
     win.show()
+
     project.read(projecteInicial)
 
 
