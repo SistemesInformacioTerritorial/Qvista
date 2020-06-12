14b2cf0cf99fc22576be8deb16f6f3af2c89f79a
JNBIMI <javier.nieva.benito@gmail.com>
2020-06-12 09:22:06 +0200

    FECHA:  12/06/2020  9:21 quito print  :(

--- moduls/QvMapeta.py
+++ moduls/QvMapeta.py
@@ -682,7 +682,7 @@ class QvMapeta(QFrame):
             margen= self.parent().parent().margen
             self.the_data.setX(event.pos().x()+margen)
             self.the_data.setY(event.pos().y()+margen)
-            print("Mapeta >> mousePressEvent emito the_data",self.the_data)  
+            # print("Mapeta >> mousePressEvent emito the_data",self.the_data)  
             self.Sig_dadoPNT.emit(self.the_data)
             return
         else:   
