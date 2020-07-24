eabd7b4029eac93a86f94a7bbfe5db914ded2080
Aleix <adalmauWea@gmail.com>
2020-07-24 13:03:38 +0200

    centrar canvas en selecció de la llista

--- Programes especifics/Selector parceles/SelectorParceles.py
+++ Programes especifics/Selector parceles/SelectorParceles.py
@@ -51,6 +51,7 @@ class SelectorParceles(QMainWindow,SelectorParcelesUi.Ui_MainWindow):
         self.layer = QgsProject.instance().mapLayersByName(capaParceles)[0]
         self.layCanvas.addWidget(self.canvas)
         self.listSel = QListWidget()
+        self.listSel.itemDoubleClicked.connect(self.clickLlista)
         self.llista_catastral = [] #contingut llista catastral
         self.llista_ids = [] #per pintar les parcel·les
 
@@ -71,6 +72,15 @@ class SelectorParceles(QMainWindow,SelectorParcelesUi.Ui_MainWindow):
         self.bConfirmar.clicked.connect(self.desarResultat) # No implementada
 
         self.carregaConfig()
+
+    def clickLlista(self,item):
+        ref_catastral = item.text()
+        index = self.llista_catastral.index(str(ref_catastral))
+        idx = self.llista_ids[index]
+        feature = self.layer.getFeature(idx)
+        rect = feature.geometry().boundingBox()
+        self.canvas.setExtent(rect)
+        self.canvas.zoomScale(500)
     
     def mouCanvas(self):
         self.tool = QgsMapToolPan(self.canvas)
@@ -83,7 +93,7 @@ class SelectorParceles(QMainWindow,SelectorParcelesUi.Ui_MainWindow):
 
     def seleccionaFeats(self,feats):
         ids=[x.id() for x in feats]
-        ref_catastre = [x[self.config['campReferencia']] for x in feats]
+        refs_catastre = [x[self.config['campReferencia']] for x in feats]
         
         for idx in ids:
             if idx in self.llista_ids:
@@ -93,7 +103,7 @@ class SelectorParceles(QMainWindow,SelectorParcelesUi.Ui_MainWindow):
         self.layer.selectByIds(self.llista_ids)
         #print(self.layer.selectedFeatureIds())
 
-        for ref in ref_catastre:
+        for ref in refs_catastre:
             if str(ref) in self.llista_catastral:
                 self.llista_catastral.remove(ref)
                 self.listSel.clear()
