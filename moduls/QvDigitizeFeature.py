83e6bb13f514d4de97095a70346c6a1b33633253
Carlos Pretel <cpretel@bcn.cat>
2020-12-22 11:55:26 +0100

    Parametro con proyecto inicial y otros...

--- moduls/QvDigitizeFeature.py
+++ moduls/QvDigitizeFeature.py
@@ -8,26 +8,73 @@ import qgis.PyQt.QtGui as qtGui
 import qgis.PyQt.QtCore as qtCor
 import qgis.PyQt.QtSql as qtSql
 
-class singleton:
+from moduls.QvSingleton import singleton
+from moduls.QvAtributs import QvFitxesAtributs
 
-    def __init__(self, cls):
-        self._cls = cls
+class QvDigitizeFeature(qgGui.QgsMapToolDigitizeFeature):
 
-    def instance(self, *args, **kwargs):
-        if hasattr(self, '_instance'):
-            return self._instance
-        else:
-            self._instance = self._cls(*args, **kwargs)
-            return self._instance
+    @classmethod
+    def new(cls, layer, canvas, attributes=None):
+        try:
+            if layer.type() == qgCor.QgsMapLayerType.VectorLayer:
+                if layer.isEditable() or layer.startEditing():
+                    dig = cls(layer, canvas, attributes)
+                    dig.newInit()
+        except Exception as e:
+            print('Error QvDigitizeFeature.new: ' + str(e))
 
-    def __call__(self, *args, **kwargs):
-        raise TypeError('Singletons must be accessed through `instance()`.')
+    def __init__(self, layer, canvas, attributes=None):
+        if len(qgGui.QgsGui.editorWidgetRegistry().factories()) == 0:
+            qgGui.QgsGui.editorWidgetRegistry().initEditors()
+        self.layer = layer
+        self.canvas = canvas
+        self.attributes = attributes
+        self.widget = QvDigitizeWidget(self.canvas)
+        super().__init__(self.canvas, self.widget)
+        self.setLayer(self.layer)
+        self.snap = QvSnapping(self.canvas)
+        # self.activate()
 
-    def __instancecheck__(self, inst):
-        return isinstance(inst, self._cls)
+    # def canvasReleaseEvent(self, e):
+    #     if e.button() == qtCor.Qt.RightButton:
+    #         print("Botón derecho 1")
+    #     super().canvasReleaseEvent(e)
 
+    # def cadCanvasReleaseEvent(self, e):
+    #     if e.button() == qtCor.Qt.RightButton:
+    #         print("Botón derecho 2")
+    #     super().cadCanvasReleaseEvent(e)
 
-@singleton        
+    def newInit(self):
+        self.digitizingCompleted.connect(self.newFeature)
+        self.digitizingFinished.connect(self.end)
+        self.canvas.setMapTool(self)
+        self.canvas.activateWindow()
+
+    def newFeature(self, feature):
+        # if self.layer.isEditable():
+        #     self.layer.commitChanges()
+            # self.layer.commitErrors()
+        print('New feature')
+        dialog = qgGui.QgsAttributeDialog(self.layer, feature, False, self.canvas)
+        dialog.setMode(qgGui.QgsAttributeEditorContext.AddFeatureMode)
+        dialog.setAttribute(qtCor.Qt.WA_DeleteOnClose)
+        if dialog.exec_() == qtWdg.QDialog.Accepted:
+            # self.feature = self.dialog.feature()
+            self.layer.commitChanges() # Si no, no se graba
+            if self.attributes is not None:
+                self.attributes.tabTaula(self.layer, True)
+                # Falta refrescar tabla datos
+        # self.canvas.unsetLastMapTool()
+
+    def end(self):
+        if self.layer.isEditable():
+            self.layer.rollBack()
+        # self.widget.clear()
+        # self.widget.hide()
+        print('End')
+
+@singleton
 class QvDigitizeWidget(qgGui.QgsAdvancedDigitizingDockWidget):
 
     def __init__(self, canvas, keys="Ctrl+4"):
@@ -37,53 +84,24 @@ class QvDigitizeWidget(qgGui.QgsAdvancedDigitizingDockWidget):
         self.shortcut = qtWdg.QShortcut(qtGui.QKeySequence(keys), self.canvas)
         self.shortcut.activated.connect(self.toggleUserVisible)
 
+class QvSnapping(qgCor.QgsSnappingConfig):
 
-class QvDigitizeFeature(qgGui.QgsMapToolDigitizeFeature):
-
-    @classmethod
-    def new(cls, layer, canvas):
-        try:
-            if layer.type() == qgCor.QgsMapLayerType.VectorLayer:
-                if not layer.isEditable():
-                    if not layer.startEditing():
-                        return
-                dig = cls(layer, canvas)
-                dig.setNew()
-        except Exception as e:
-            print('Error QvDigitizeFeature.new: ' + str(e))
-
-    def __init__(self, layer, canvas):
-        self.layer = layer
+    def __init__(self, canvas):
+        super().__init__()
         self.canvas = canvas
-        self.widget = QvDigitizeWidget.instance(self.canvas)
-        super().__init__(self.canvas, self.widget)
-        self.setLayer(self.layer)
-
-    def setNew(self):
-        self.digitizingCompleted.connect(self.newFeature)
-        self.digitizingFinished.connect(self.end)
-        self.canvas.setMapTool(self)
-        self.canvas.activateWindow()
-        self.canvas.setFocus()
-
-    def newFeature(self, feature):
-        print('New feature')
-        if self.isActive():
-            self.canvas.unsetMapTool(self)
-        if self.layer.isEditable():
-            self.layer.rollBack()
-        # ok = self.layer.commitChanges()
-        # self.layer.commitErrors()
-        # self.layer.rollBack()
-
-    def end(self):
-        if self.layer.isEditable():
-            self.layer.rollBack()
-        # self.widget.clear()
-        # self.widget.hide()
-        print('End')
+        self.setEnabled(True)
+        self.setType(qgCor.QgsSnappingConfig.VertexAndSegment)
+        self.setUnits(qgCor.QgsTolerance.Pixels)
+        self.setTolerance(10)
+        self.setMode(qgCor.QgsSnappingConfig.AllLayers)
+        self.setIntersectionSnapping(True)
+        self.snapUtils = qgGui.QgsMapCanvasSnappingUtils(self.canvas)
+        self.snapUtils.setConfig(self)
+        self.canvas.setSnappingUtils(self.snapUtils)
 
 if __name__ == "__main__":
+
+    # https://anitagraser.com/pyqgis-101-introduction-to-qgis-python-programming-for-non-programmers/pyqgis101-creating-editing-a-new-vector-layer/
 
     from qgis.core.contextmanagers import qgisapp
     from moduls.QvApp import QvApp
@@ -105,22 +123,22 @@ if __name__ == "__main__":
         leyenda.readProject(inicial)
 
         leyenda.setWindowTitle('Llegenda')
-        # leyenda.setGeometry(50, 50, 400, 600)
+        leyenda.setGeometry(100, 50, 400, 600)
         leyenda.show()
 
         canvas.setWindowTitle('Canvas - ' + inicial)
-        # canvas.setGeometry(400, 50, 900, 600)
+        canvas.setGeometry(510, 50, 900, 600)
         canvas.show()
 
         atributos.setWindowTitle('Atributs')
-        # atributos.setGeometry(50, 500, 1050, 250)
+        atributos.setGeometry(100, 700, 1310, 300)
         leyenda.obertaTaulaAtributs.connect(atributos.show)
 
         # Acciones de usuario para el menú
 
         def testDigitize():
             print('ini test digitize')
-            QvDigitizeFeature.new(leyenda.currentLayer(), canvas)
+            QvDigitizeFeature.new(leyenda.currentLayer(), canvas, atributos)
             print('fin test digitize')
 
         def writeProject():
@@ -172,112 +190,3 @@ if __name__ == "__main__":
         # Conexión de la señal con la función menuContexte para personalizar el menú
 
         leyenda.clicatMenuContexte.connect(menuContexte)
-
-
-# /***************************************************************************
-#     qgsmaptooladdfeature.cpp
-#     ------------------------
-#     begin                : April 2007
-#     copyright            : (C) 2007 by Marco Hugentobler
-#     email                : marco dot hugentobler at karto dot baug dot ethz dot ch
-#  ***************************************************************************
-#  *                                                                         *
-#  *   This program is free software; you can redistribute it and/or modify  *
-#  *   it under the terms of the GNU General Public License as published by  *
-#  *   the Free Software Foundation; either version 2 of the License, or     *
-#  *   (at your option) any later version.                                   *
-#  *                                                                         *
-#  ***************************************************************************/
-
-# #include "qgsmaptooladdfeature.h"
-# #include "qgsadvanceddigitizingdockwidget.h"
-# #include "qgsapplication.h"
-# #include "qgsattributedialog.h"
-# #include "qgsexception.h"
-# #include "qgscurvepolygon.h"
-# #include "qgsfields.h"
-# #include "qgsgeometry.h"
-# #include "qgslinestring.h"
-# #include "qgsmultipoint.h"
-# #include "qgsmapcanvas.h"
-# #include "qgsmapmouseevent.h"
-# #include "qgspolygon.h"
-# #include "qgsproject.h"
-# #include "qgsvectordataprovider.h"
-# #include "qgsvectorlayer.h"
-# #include "qgslogger.h"
-# #include "qgsfeatureaction.h"
-# #include "qgisapp.h"
-# #include "qgsexpressioncontextutils.h"
-# #include "qgsrubberband.h"
-
-# #include <QSettings>
-
-# QgsMapToolAddFeature::QgsMapToolAddFeature( QgsMapCanvas *canvas, CaptureMode mode )
-#   : QgsMapToolDigitizeFeature( canvas, QgisApp::instance()->cadDockWidget(), mode )
-#   , mCheckGeometryType( true )
-# {
-#   setLayer( canvas->currentLayer() );
-
-#   mToolName = tr( "Add feature" );
-#   connect( QgisApp::instance(), &QgisApp::newProject, this, &QgsMapToolAddFeature::stopCapturing );
-#   connect( QgisApp::instance(), &QgisApp::projectRead, this, &QgsMapToolAddFeature::stopCapturing );
-# }
-
-# bool QgsMapToolAddFeature::addFeature( QgsVectorLayer *vlayer, const QgsFeature &f, bool showModal )
-# {
-#   QgsFeature feat( f );
-#   QgsExpressionContextScope *scope = QgsExpressionContextUtils::mapToolCaptureScope( snappingMatches() );
-#   QgsFeatureAction *action = new QgsFeatureAction( tr( "add feature" ), feat, vlayer, QString(), -1, this );
-#   if ( QgsRubberBand *rb = takeRubberBand() )
-#     connect( action, &QgsFeatureAction::addFeatureFinished, rb, &QgsRubberBand::deleteLater );
-#   bool res = action->addFeature( QgsAttributeMap(), showModal, scope );
-#   if ( showModal )
-#     delete action;
-#   return res;
-# }
-
-# void QgsMapToolAddFeature::digitized( const QgsFeature &f )
-# {
-#   QgsVectorLayer *vlayer = currentVectorLayer();
-#   bool res = addFeature( vlayer, f, false );
-
-#   if ( res )
-#   {
-#     //add points to other features to keep topology up-to-date
-#     bool topologicalEditing = QgsProject::instance()->topologicalEditing();
-#     QgsProject::AvoidIntersectionsMode avoidIntersectionsMode = QgsProject::instance()->avoidIntersectionsMode();
-#     if ( topologicalEditing && avoidIntersectionsMode == QgsProject::AvoidIntersectionsMode::AvoidIntersectionsLayers &&
-#          ( mode() == CaptureLine || mode() == CapturePolygon ) )
-#     {
-
-#       //use always topological editing for avoidIntersection.
-#       //Otherwise, no way to guarantee the geometries don't have a small gap in between.
-#       const QList<QgsVectorLayer *> intersectionLayers = QgsProject::instance()->avoidIntersectionsLayers();
-
-#       if ( !intersectionLayers.isEmpty() ) //try to add topological points also to background layers
-#       {
-#         for ( QgsVectorLayer *vl : intersectionLayers )
-#         {
-#           //can only add topological points if background layer is editable...
-#           if ( vl->geometryType() == QgsWkbTypes::PolygonGeometry && vl->isEditable() )
-#           {
-#             vl->addTopologicalPoints( f.geometry() );
-#           }
-#         }
-#       }
-#     }
-#     if ( topologicalEditing )
-#     {
-#       QList<QgsPointLocator::Match> sm = snappingMatches();
-#       for ( int i = 0; i < sm.size() ; ++i )
-#       {
-#         if ( sm.at( i ).layer() )
-#         {
-#           sm.at( i ).layer()->addTopologicalPoints( f.geometry().vertexAt( i ) );
-#         }
-#       }
-#       vlayer->addTopologicalPoints( f.geometry() );
-#     }
-#   }
-# }
