05a0fe3dd6afa2c66e3e2b641e45cbf26eec6f95
CPCIMI <cpretel@bcn.cat>
2024-08-01 12:06:13 +0200

    Prueba widget navgación temporal

---
+++ moduls/QvTemporalNavigation.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+# from qgis.core import QgsProject, QgsLayoutExporter, QgsReport, QgsFeedback
+# from qgis.PyQt.QtCore import Qt
+
+from qgis.gui import QgsTemporalControllerWidget
+
+from moduls.QvFuncions import debugging
+import os 
+
+
+if __name__ == "__main__":
+
+    from qgis.core.contextmanagers import qgisapp
+    from qgis.core import QgsLayoutExporter, QgsReport, QgsApplication
+    from qgis.gui import QgsMapCanvas
+    from moduls.QvLlegenda import QvLlegenda
+    from moduls.QvApp import QvApp
+    from moduls.QvAtributs import QvAtributs
+    from DataPlotly.QvDataPlotly import QvDataPlotly
+    from qgis.analysis import QgsNativeAlgorithms
+    from qgis.PyQt.QtWidgets import QWidget, QHBoxLayout, QPushButton
+    
+    import sys
+
+
+
+    with qgisapp() as app:
+
+        qApp = QvApp()
+        qApp.carregaIdioma(app, 'ca')
+
+        canvas = QgsMapCanvas()
+
+        atributs = QvAtributs(canvas)
+
+        llegenda = QvLlegenda(canvas, atributs)
+
+        # sys.path.append(r'C:\OSGeo4W\apps\qgis-ltr\python\plugins')
+        # import processing
+
+        # nAlgs = QgsNativeAlgorithms()
+        # nAlgs.loadAlgorithms()
+        # QgsApplication.processingRegistry().addProvider(nAlgs) 
+
+        # llegenda.project.read('projectes/Illes.qgs')
+        llegenda.project.read("D:/qVista/EndrecaSoroll/sorollEndreca.qgs")
+
+        llegenda.setWindowTitle('Llegenda')
+        llegenda.setGeometry(50, 50, 300, 400)
+        llegenda.show()
+
+        canvas.setWindowTitle('Mapa')
+        canvas.setGeometry(400, 50, 700, 400)
+        canvas.show()
+
+        # Abrimos una tabla de atributos
+        # layer = llegenda.capaPerNom('Illes')
+        # atributs.obrirTaula(layer)
+
+        # atributs.setWindowTitle('Atributs')
+        # atributs.setGeometry(50, 500, 1050, 250)
+        # llegenda.obertaTaulaAtributs.connect(atributs.show)
+
+        controller = QgsTemporalControllerWidget() 
+        controler_to_be_used_by_canvas =  controller.temporalController()
+        canvas.setTemporalController(controler_to_be_used_by_canvas)
+        controller.setWindowTitle('Navegació temporal')
+        controller.setGeometry(50, 500, 1050, 150)
+        controller.show()
+
+        # widget = QWidget()
+        # push_button = QPushButton("Click me!")
+        # layout = QHBoxLayout()
+        # layout.addWidget(controller)        
+        # widget.setLayout(layout)
+        # widget.setWindowTitle('Navegació temporal')
+        # widget.show()
