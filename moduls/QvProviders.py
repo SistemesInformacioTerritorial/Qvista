f933ecfe827b518bca3515a2ae9f53f7e3b55db3
CPCIMI <cpretel@bcn.cat>
2025-07-03 10:35:06 +0200

    Cambio a directorio models

--- moduls/QvProviders.py
+++ moduls/QvProviders.py
@@ -41,7 +41,7 @@ class QvQvistaProvider(QgsProcessingProvider):
     # Directorio donde se encuentran los modelos disponibles en qVista
     # Los ficheros .model3 han de copiarse siempre a traves de GitHub
     # para que se distribuyan con qVista
-    MODELS_FOLDER = str(os.path.join(os.getcwd(), r"processos\models"))
+    MODELS_FOLDER = str(os.path.join(os.getcwd(), r"models"))
     # Nombre de grupo o lista de grupos permitidos (en minúsculas)
     MODELS_GROUPS_FILTER = None # Se cargan todos, pueden filtrarse después
 
